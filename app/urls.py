6c586cc699392e2e8ba0b13b6230398b4a72ecda
18031J0043 <akhilsangubotla@msitprogram.net>
2019-11-01 15:24:43 +0530

    First commit

--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,8 @@ from .import views
 
 urlpatterns=[
     # path('',views.home,name="home"),
-    path('', views.HomePageView.as_view(), name="home"),
+    path('', views.PageView, name=""),
+    path('home', views.HomePageView.as_view(), name="home"),
     # path('detail/<int:id>/',views.detail,name="detail"),
     path('detail/<int:pk>/', views.ContactDetailView.as_view(), name="detail"),
     path('search/', views.search, name= "search"),
